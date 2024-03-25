ce4e8ab1b1127bde2dfc9cf4e5e1dc464965195a
Tongxuan Liu <tongxuan.ltx@mail.ustc.edu.cn>
2024-03-25 02:02:34 +0000

    [ut] add more unit tests to cover spec kernels behavious.

--- src/layers/attention/attention_test.cpp
+++ src/layers/attention/attention_test.cpp
@@ -336,4 +336,241 @@ INSTANTIATE_TEST_SUITE_P(
         ::testing::Values(1)                                 // num_splits
         ));
 
+class AttentionValidateTest
+    : public ::testing::TestWithParam<std::tuple<torch::Device,
+                                                 torch::ScalarType,
+                                                 int64_t /*batch_size*/,
+                                                 int64_t /*block_size*/,
+                                                 int64_t /*q_max_seq_len*/,
+                                                 int64_t /*kv_max_seq_len*/,
+                                                 int64_t /*n_heads*/,
+                                                 int64_t /*n_kv_heads*/,
+                                                 int64_t /*head_dim*/,
+                                                 float /*scale*/,
+                                                 bool /*alibi*/,
+                                                 int32_t /*num_splits*/>> {};
+TEST_P(AttentionValidateTest, OneSequenceValidateTestBasic) {
+  const auto& [device,
+               dtype,
+               batch_size,
+               block_size,
+               q_max_seq_len,
+               kv_max_seq_len,
+               n_heads,
+               n_kv_heads,
+               head_dim,
+               scale,
+               alibi,
+               num_splits] = GetParam();
+  // make sure kv_max_seq_len >= q_max_seq_len
+  if (kv_max_seq_len < q_max_seq_len) {
+    GTEST_SKIP() << "kv_max_seq_len < q_max_seq_len";
+  }
+  // total number of blocks: batch_size * max_n_blocks_per_seq
+  const int32_t n_blocks =
+      (kv_max_seq_len + block_size - 1) / block_size * batch_size * 2;
+  // assign block ids for each sequence randomly
+  std::vector<int32_t> available_block_ids(n_blocks);
+  for (int32_t i = 0; i < n_blocks; ++i) {
+    available_block_ids[i] = i;
+  }
+  std::shuffle(
+      available_block_ids.begin(), available_block_ids.end(), std::mt19937());
+  const int32_t max_n_blocks_per_seq =
+      (kv_max_seq_len + block_size - 1) / block_size;
+
+  std::vector<std::vector<int32_t>> block_tables_vec;
+  std::vector<int> slot_ids;
+
+  // generate random seq lens with size in [1, q/kv_max_seq_len]
+  std::vector<int32_t> q_cu_seq_lens_vec = {0};
+  std::vector<int32_t> k_cu_seq_lens_vec = {0};
+  int32_t n_kv_tokens = 0;
+  int32_t n_q_tokens = 0;
+  absl::BitGen gen;
+  for (int i = 0; i < batch_size; ++i) {
+    // q_len: [1, q_max_seq_len]
+    const int32_t q_len =
+        absl::Uniform<int>(absl::IntervalClosedClosed, gen, 1, q_max_seq_len);
+    // const int32_t q_len = q_max_seq_len;
+    n_q_tokens += q_len;
+    q_cu_seq_lens_vec.push_back(n_q_tokens);
+
+    // kv_len >= q_len
+    int32_t kv_len = q_len;
+    if (q_len < kv_max_seq_len) {
+      // sample kv_len from [q_len, kv_max_seq_len]
+      kv_len = absl::Uniform<int>(
+          absl::IntervalClosedClosed, gen, q_len, kv_max_seq_len);
+    }
+    // const int32_t kv_len = kv_max_seq_len;
+    n_kv_tokens += kv_len;
+    k_cu_seq_lens_vec.push_back(n_kv_tokens);
+
+    // assign blocks for each sequence
+    std::vector<int32_t> block_table(max_n_blocks_per_seq);
+    const int32_t n_blocks_per_seq = (kv_len + block_size - 1) / block_size;
+    for (int j = 0; j < n_blocks_per_seq; ++j) {
+      ASSERT_FALSE(available_block_ids.empty());
+      block_table[j] = available_block_ids.back();
+      available_block_ids.pop_back();
+    }
+    block_tables_vec.push_back(block_table);
+
+    // assign slots for each sequence
+    for (int j = 0; j < kv_len; ++j) {
+      const int32_t block_id = block_table[j / block_size];
+      const int32_t block_offset = j % block_size;
+      slot_ids.push_back(block_id * block_size + block_offset);
+    }
+  }
+
+  ASSERT_EQ(block_tables_vec.size(), batch_size);
+  ASSERT_EQ(slot_ids.size(), n_kv_tokens);
+
+  // allocate memory for input tensors
+  const auto options = torch::dtype(dtype).device(device);
+
+  // generate query, key and value
+  torch::Tensor query = torch::rand({n_q_tokens, n_heads, head_dim}, options);
+  torch::Tensor key = torch::rand({n_kv_tokens, n_kv_heads, head_dim}, options);
+  torch::Tensor value =
+      torch::rand({n_kv_tokens, n_kv_heads, head_dim}, options);
+
+  // construct key and value cache
+  const std::vector<int64_t> kv_shape = {
+      n_blocks, block_size, n_kv_heads, head_dim};
+  torch::Tensor k_cache = torch::empty(kv_shape, options);
+  torch::Tensor v_cache = torch::empty(kv_shape, options);
+
+  // set key and value into cache based on slot_ids
+  set_kv_cache(slot_ids, key, value, k_cache, v_cache);
+  auto [k, v] = get_kv_cache(slot_ids, k_cache, v_cache);
+  ASSERT_TRUE(torch::equal(k, key));
+  ASSERT_TRUE(torch::equal(v, value));
+
+  torch::Tensor q_cu_seq_lens = torch::tensor(
+      q_cu_seq_lens_vec, torch::dtype(torch::kInt32).device(device));
+  torch::Tensor k_cu_seq_lens = torch::tensor(
+      k_cu_seq_lens_vec, torch::dtype(torch::kInt32).device(device));
+
+  // construct block tables with padding=0
+  auto block_tables = torch::empty(
+      {static_cast<int32_t>(block_tables_vec.size()), max_n_blocks_per_seq},
+      torch::dtype(torch::kInt32).device(device));
+  for (int64_t i = 0; i < block_tables_vec.size(); ++i) {
+    block_tables.index_put_({i, ISlice()},
+                            torch::tensor(block_tables_vec[i], torch::kInt));
+  }
+
+  torch::optional<torch::Tensor> alibi_slopes;
+  if (alibi) {
+    alibi_slopes =
+        torch::rand({n_heads}, torch::dtype(torch::kFloat32).device(device));
+  }
+
+  InputParameters input_params;
+  input_params.q_cu_seq_lens = q_cu_seq_lens;
+  input_params.kv_cu_seq_lens = k_cu_seq_lens;
+  input_params.q_max_seq_len = q_max_seq_len;
+  input_params.kv_max_seq_len = kv_max_seq_len;
+
+  RefHandler ref_handler(scale, alibi_slopes);
+  torch::Tensor ref_output = torch::empty_like(query);
+  ref_handler.batch_prefill(query, key, value, input_params, ref_output);
+
+  // flash attn handler
+  FlashAttnHandler flash_attn_handler(scale, alibi_slopes);
+  torch::Tensor output = torch::empty_like(query);
+  flash_attn_handler.batch_prefill(query, key, value, input_params, output);
+  EXPECT_TRUE(
+      torch::allclose(ref_output, output, /*rtol=*/1e-2, /*atol=*/1e-3));
+
+  torch::Tensor output_with_cache = torch::empty_like(query);
+
+  input_params.block_tables = block_tables;
+  flash_attn_handler.batch_decode(
+      query, {k_cache, v_cache}, input_params, output_with_cache);
+
+  EXPECT_TRUE(
+      torch::allclose(output, output_with_cache, /*rtol=*/1e-2, /*atol=*/1e-3));
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    OneSequenceValidateTestBasic,
+    AttentionValidateTest,
+    ::testing::Combine(
+        ::testing::Values(torch::kCUDA),
+        ::testing::Values(torch::kHalf, torch::kBFloat16),
+        ::testing::Values(1),                                // batch_size
+        ::testing::Values(256),                              // block_size
+        ::testing::Values(10),                               // q_max_seq_len
+        ::testing::Values(100),                              // kv_max_seq_len
+        ::testing::Values(6),                                // n_heads
+        ::testing::Values(6),                                // n_kv_heads
+        ::testing::Values(32),                               // head_dim
+        ::testing::Values(0.9),                              // scale
+        ::testing::Values(false),                            // alibi
+        ::testing::Values(1)                                 // num_splits
+        ));
+
+TEST(AttentionSpeculativeTest, LLMPrefillTest) {
+  torch::Device device(torch::kCUDA);
+  torch::ScalarType dtype(torch::kHalf, torch::kBFloat16);
+  int64_t block_size = 256;
+  int64_t q_max_seq_len = 10;
+  int64_t kv_max_seq_len = 100;
+  int64_t n_heads = 6;
+  int64_t n_kv_heads = 6;
+  int64_t head_dim = 32;
+  float scale = 0.9;
+  const int32_t n_blocks =
+    (kv_max_seq_len + block_size - 1) / block_size * 2;
+  std::vector<int32_t> available_block_ids(n_blocks);
+  for (int32_t i = 0; i < n_blocks; ++i) {
+    available_block_ids[i] = i;
+  }
+  std::shuffle(
+      available_block_ids.begin(), available_block_ids.end(), std::mt19937());
+  const int32_t max_n_blocks_per_seq =
+      (kv_max_seq_len + block_size - 1) / block_size;
+
+  std::vector<std::vector<int32_t>> block_tables_vec;
+  std::vector<int> slot_ids;
+
+  std::vector<int32_t> q_cu_seq_lens_vec = {0};
+  std::vector<int32_t> k_cu_seq_lens_vec = {0};
+  int32_t n_kv_tokens = 0;
+  int32_t n_q_tokens = 0;
+  absl::BitGen gen;
+  const int32_t q_len =
+      absl::Uniform<int>(absl::IntervalClosedClosed, gen, 1, q_max_seq_len);
+  n_q_tokens += q_len;
+  q_cu_seq_lens_vec.push_back(n_q_tokens);
+  int32_t kv_len = q_len;
+  if (q_len < kv_max_seq_len) {
+    kv_len = absl::Uniform<int>(
+        absl::IntervalClosedClosed, gen, q_len, kv_max_seq_len);
+  }
+  n_kv_tokens += kv_len;
+  k_cu_seq_lens_vec.push_back(n_kv_tokens);
+
+  std::vector<int32_t> block_table(max_n_blocks_per_seq);
+  const int32_t n_blocks_per_seq = (kv_len + block_size - 1) / block_size;
+
+  for (int j = 0; j < n_blocks_per_seq; ++j) {
+    ASSERT_FALSE(available_block_ids.empty());
+    block_table[j] = available_block_ids.back();
+    available_block_ids.pop_back();
+  }
+  block_tables_vec.push_back(block_table);
+
+  for (int j = 0; j < kv_len; ++j) {
+    const int32_t block_id = block_table[j / block_size];
+    const int32_t block_offset = j % block_size;
+    slot_ids.push_back(block_id * block_size + block_offset);
+  }
+
+}
+
 }  // namespace llm
